7dfba6d1e22619f362214d0efaee680830f6291f
Lily Springer <lilyspringer@hotmail.com>
2023-07-16 16:49:00 -0600

    display countries working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { useSelector } from 'react-redux'
 import "./App.css";
 import Header from "./Components/Header";
 import MainDisplay from "./Components/MainDisplay";
 import OptionDisplay from "./Components/OptionDisplay";
+import { useSelector } from "react-redux";
 import { selectPotentials } from "./redux/slices/potentialCountriesSlice";
+import { selectDisplay } from "./redux/slices/displayCountrySlice";
 
 function App() {
-    const potentials = useSelector(selectPotentials)
-    console.log(potentials)
-    return (
-        <div className="App font-link">
-            <Header />
-            <OptionDisplay />
-        </div>
-    );
+  const potentials = useSelector(selectPotentials);
+  const currentDisplay = useSelector(selectDisplay);
+
+  console.log("POTENTIALS", potentials);
+  console.log("DISPLAY", currentDisplay);
+
+  return (
+    <div className="App font-link">
+      <Header />
+      {currentDisplay ? <MainDisplay /> : <OptionDisplay />}
+    </div>
+  );
 }
 
 export default App;
